8883fb352d3a2aa52f5dd7819d5c0e74eaac97ce
Kyungjin Cho <56642855+Kyungjin-Cho@users.noreply.github.com>
2024-03-30 16:15:48 +0000

    Add files via upload

---
+++ Python/datatypeOperators.py
@@ -0,0 +1,19 @@
+# Expressions = Values + Operators
+# Integer(Int), Float, String
+print(2.0 + 2.0)
+print(5 - 3)
+print(3 * 7)
+print(22 / 7)
+print(2 + 3 * 6)
+print((2 + 3) * 6)
+
+print('Alice' + 'Bob')
+print('Alice' * 3)
+
+# Variable
+spam = 42
+print(spam)
+
+# Overwritting varibale(42 -> Hello)
+spam = 'Hello'
+print(spam + ' World')
